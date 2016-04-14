30cd86ccd037585ddaf560c18a663d0d05ffbbe3
sunya <mugen.xyz@gmail.com>
2016-04-15 04:25:40 +0900

    Delete unused file

--- utils.js
+++
@@ -1,5 +0,0 @@
-exports.loop = function loop(promise, fn) {
-  return promise.then(fn).then(wrapper => {
-    return !wrapper.done ? loop(Promise.resolve(wrapper.value), fn): wrapper.value;
-  });
-};

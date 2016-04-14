76aed2c48b9e371e2e420d577c1254a708e79824
sunya <mugen.xyz@gmail.com>
2016-04-15 04:42:44 +0900

    Add isInitialized state

--- reducers/users.js
+++ reducers/users.js
@@ -2,16 +2,17 @@ import { FETCH_USERS, RECEIVE_USERS, FAIL_USERS } from '../constants/users';
 
 export default (state = {
   isFetching: false,
-  users: [],
+  isInitialized: false,
+  users: {},
   error: null
 }, action) => {
   switch(action.type) {
   case FETCH_USERS:
     return Object.assign({}, state, {isFetching: true});
   case RECEIVE_USERS:
-    return Object.assign({}, state, {isFetching: false, users: action.users});
+    return Object.assign({}, state, {isFetching: false, isInitialized: true, users: action.users});
   case FAIL_USERS:
-  return Object.assign({}, state, {isFetching: false, error: action.error});
+    return Object.assign({}, state, {isFetching: false, error: action.error});
   default:
     return state;
   }

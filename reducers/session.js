dfe4dcf00e05c70a2c5102a339cd4bbe36ff1119
sunya <mugen.xyz@gmail.com>
2016-04-15 04:42:09 +0900

    Fix constant names

--- reducers/session.js
+++ reducers/session.js
@@ -1,18 +1,22 @@
-import { FETCH_LOGIN, RECEIVE_LOGIN, REQUEST_LOGOUT, RECEIVE_LOGOUT } from '../constants/session';
+import { FETCH_LOGIN_STATUS, RECEIVE_LOGIN_STATUS, FAILED_LOGIN_STATUS, REQUEST_LOGOUT, SUCCESS_LOGOUT, FAILED_LOGOUT } from '../constants/session';
 
 export default (state = {
   isFetching: false,
   isLogin: false
 }, action) => {
   switch(action.type) {
-  case FETCH_LOGIN:
+  case FETCH_LOGIN_STATUS:
     return Object.assign({}, state, {isFetching: true});
-  case RECEIVE_LOGIN:
+  case RECEIVE_LOGIN_STATUS:
     return Object.assign({}, state, {isFetching: false, isLogin: action.isLogin});
+  case FAILED_LOGIN_STATUS:
+    return Object.assign({}, state, {isFetching: false});
   case REQUEST_LOGOUT:
     return Object.assign({}, state, {isFetching: true});
-  case RECEIVE_LOGOUT:
+  case SUCCESS_LOGOUT:
     return Object.assign({}, state, {isFetching: false, isLogin: action.ok});
+  case FAILED_LOGOUT:
+    return Object.assign({}, state, {isFetching: false});
   default:
     return state;
   }

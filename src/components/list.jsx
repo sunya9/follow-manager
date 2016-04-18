b22890e1f87bfe7828f0b99c349e3681cfc2d316
sunya <mugen.xyz@gmail.com>
2016-04-18 10:12:23 +0900

    Show followers and others

--- src/components/list.jsx
+++ src/components/list.jsx
@@ -5,14 +5,27 @@ import { connect } from 'react-redux';
 
 class List extends Component {
   render() {
-    const rows = [];
-    if(this.props.users.friends) {
-      this.props.users.friends.forEach(id => {
-        // console.log(user);
-        rows.push(
-            <User key={id} userId={id} />
-        );
-      });
+    let rows = [];
+    switch(this.props.type) {
+    case 'selected':
+      rows = Object.keys(this.props.users.allUserInfo)
+        .filter(id => this.props.users.allUserInfo[id].select)
+        .map(generateUser);
+      break;
+    case 'friends':
+      rows = this.props.users.friends.map(generateUser);
+      break;
+    case 'followers':
+      rows = this.props.users.followers.map(generateUser);
+      break;
+    case 'kataomoi':
+      rows = this.props.users.friends.filter(id => !this.props.users.allUserInfo[id].connections.includes('followed_by'))
+        .map(generateUser);
+      break;
+    case 'kataomoware':
+      rows = this.props.users.followers.filter(id => !this.props.users.allUserInfo[id].connections.includes('following'))
+        .map(generateUser);
+      break;
     }
 
     return (
@@ -25,6 +38,14 @@ class List extends Component {
     );
   }
 }
+
+function generateUser(id) {
+  return <User key={id} userId={+id} />;
+}
+
+List.propTypes = {
+  type: PropTypes.oneOf(['selected', 'friends', 'followers', 'kataomoi', 'kataomoware']).isRequired
+};
 
 function mapStateToProps(state) {
   return state.users;

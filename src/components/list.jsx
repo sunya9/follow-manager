ce58e7c7e36373390e3c521547d57ffa1c62d7e1
sunya <mugen.xyz@gmail.com>
2016-04-19 22:40:18 +0900

    Add select all

--- src/components/list.jsx
+++ src/components/list.jsx
@@ -1,46 +1,86 @@
 import React, { Component, PropTypes } from 'react';
 import User from './user';
-import { getUsersIfNeeded } from '../../actions/users';
 import { connect } from 'react-redux';
 
+import { selectAll } from '../../actions/users';
+
 class List extends Component {
+  constructor(props) {
+    super(props);
+    this.exist = this.exist.bind(this);
+  }
+  exist(id) {
+    return this.props.users.users.allUserInfo[id].select;
+  }
   render() {
-    let rows = [];
+    let showUsers;
     switch(this.props.type) {
     case 'selected':
-      rows = Object.keys(this.props.users.allUserInfo)
-        .filter(id => this.props.users.allUserInfo[id].select)
-        .map(generateUser);
+      showUsers = Object.keys(this.props.users.users.allUserInfo)
+        .filter(id => this.props.users.users.allUserInfo[id].select);
       break;
     case 'friends':
-      rows = this.props.users.friends.map(generateUser);
+      showUsers = this.props.users.users.friends;
       break;
     case 'followers':
-      rows = this.props.users.followers.map(generateUser);
+      showUsers = this.props.users.users.followers;
       break;
     case 'kataomoi':
-      rows = this.props.users.friends.filter(id => !this.props.users.allUserInfo[id].connections.includes('followed_by'))
-        .map(generateUser);
+      showUsers = this.props.users.users.friends.filter(id => !this.props.users.users.allUserInfo[id].connections.includes('followed_by'))
       break;
     case 'kataomoware':
-      rows = this.props.users.followers.filter(id => !this.props.users.allUserInfo[id].connections.includes('following'))
-        .map(generateUser);
+      showUsers = this.props.users.users.followers.filter(id => !this.props.users.users.allUserInfo[id].connections.includes('following'))
       break;
     }
-
-    return (
+    const rows = showUsers.map(generateUser);
+    const isEmpty = rows.length === 0;
+    const empty = 'ユーザは見つかりませんでした。';
+    return this.props.settings.showMode === 'card' ? (
       <div>
-        <button className="btn btn-default" onClick={this.props.getUsers}>再取得</button>
-        <div className="card-deck">
+        {isEmpty ? (<p>{empty}</p>) : (
+          <ul className="card-deck list-unstyled">
           {rows}
-        </div>
+          </ul>
+        )}
+      </div>
+    ) : (
+      <div className="table-responsive">
+        <table className="table table-bordered table-hover user-list-table">
+          <thead>
+            <tr>
+              <th>
+                <input type="checkbox" onChange={() => this.props.selectAll(showUsers)} disdabled={showUsers.length > 0} checked={showUsers.length > 0 && showUsers.every(this.exist)} ref={input => {
+                  if (input) {
+                    const count = showUsers.filter(id => this.props.users.users.allUserInfo[id].select).length;
+                    input.indeterminate = showUsers.length > 0 && 0 < count && count < showUsers.length;
+                  }
+                }} />
+              </th>
+              <th className="user-list-table__index">#</th>
+              <th>Name</th>
+              <th>Screen Name</th>
+              <th>Following</th>
+              <th>Followers</th>
+              <th>Last-update</th>
+            </tr>
+          </thead>
+          <tbody>
+            {isEmpty ? (
+              <tr>
+                <td colSpan="7">
+                  {empty}
+                </td>
+              </tr>
+            ) : rows}
+          </tbody>
+        </table>
       </div>
     );
   }
 }
 
-function generateUser(id) {
-  return <User key={id} userId={+id} />;
+function generateUser(id, index) {
+  return <User key={id} userId={+id} index={index+1} />;
 }
 
 List.propTypes = {
@@ -48,15 +88,18 @@ List.propTypes = {
 };
 
 function mapStateToProps(state) {
-  return state.users;
+  return {
+    users: state.users,
+    settings: state.settings
+  };
 }
 
-const mapDispatchToProps = (dispatch) => {
+function mapDispatchToProps(dispatch) {
   return {
-    getUsers() {
-      dispatch(getUsersIfNeeded());
+    selectAll(showUsers) {
+      dispatch(selectAll(showUsers));
     }
   };
-};
+}
 
 export default connect(mapStateToProps, mapDispatchToProps)(List);

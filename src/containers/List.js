876070a248c86bb283e69f1e722caf9b131fdb9d
sunya <mugen.xyz@gmail.com>
2016-04-27 22:45:35 +0900

    Add filter component and sort component

--- src/containers/List.js
+++ src/containers/List.js
@@ -1,51 +1,100 @@
 import React, { Component, PropTypes } from 'react';
 import User from './user';
 import { connect } from 'react-redux';
-
 import { selectAll } from '../actions/users';
 import Checkbox from '../components/Checkbox';
+import SortThButton from '../containers/SortThButton';
+
+const dateTest = /_at$/;
 
 class List extends Component {
   constructor(props) {
     super(props);
     this.exist = this.exist.bind(this);
     this.generateUser = this.generateUser.bind(this);
+    this.filterUser = this.filterUser.bind(this);
+    this.sort = this.sort.bind(this);
   }
   exist(id) {
-    return this.props.users.users.allUserInfo[id].select;
+    return this.props.users.allUserInfo[id].select;
   }
   generateUser(id, index) {
-    return <User key={id} userId={+id} index={index + 1} />;
+    return <User key={id} userId={id} index={index + 1} />;
   }
+  filterUser(id) {
+    const user = this.props.users.allUserInfo[id];
+    const matchId = user.screen_name.indexOf(this.props.filter) >= 0;
+    const matchName = user.name.indexOf(this.props.filter) >= 0;
+    return matchId || matchName;
+  }
+  sort(a, b) {
+    a = this.props.users.allUserInfo[a];
+    b = this.props.users.allUserInfo[b];
+    const path = this.props.settings.sort.key.split('.');
+    while(path.length > 0 && a && b) {
+      const key = path.shift();
+      a = a[key];
+      b = b[key];
+      if(dateTest.test(key)) {
+        a = new Date(a).getTime();
+        b = new Date(b).getTime();
+      }
+    }
+    let res = 0;
+    if(a && b && typeof a === 'string') {
+      a = a.toLowerCase();
+      b = b.toLowerCase();
+    }
+    res = (a > b ? 1 : -1);
+    if(path.length > 0) {
+      // error: undefined a or b
+      res = typeof a === 'undefined' ? 1 : -1;
+    }
+    return res * (this.props.settings.sort.reverse ? -1 : 1);
+  }
+
   render() {
-    const rows = this.props.showUsers.map(this.generateUser);
+    const filteredUsers = (
+      this.props.filter
+        ? this.props.showUsers.filter(this.filterUser)
+        : this.props.showUsers
+      );
+    if(this.props.settings.sort.key) {
+      filteredUsers.sort(this.sort);
+    } else if(this.props.settings.sort.reverse) {
+      filteredUsers.reverse();
+    }
+    const rows = filteredUsers.map(this.generateUser);
     const isEmpty = rows.length === 0;
-    const empty = 'ユーザは見つかりませんでした。';
+    const empty = this.props.usersInitialized
+      ? 'ユーザは見つかりませんでした。'
+      : (
+        <span>
+          <i className="fa fa-spinner fa-spin"></i>&nbsp;取得中です…。
+        </span>
+      );
     return this.props.settings.showMode === 'card' ? (
-      <div>
-        {isEmpty ? (<p>{empty}</p>) : (
+        isEmpty ? (<p>{empty}</p>) : (
           <ul className="card-deck list-unstyled">
           {rows}
           </ul>
-        )}
-      </div>
+        )
     ) : (
       <div className="table-responsive">
-        <table className="table table-bordered table-hover user-list-table">
+        <table className="table table-hover user-list-table">
           <thead>
             <tr>
-              <th>
-                <Checkbox onChange={() => this.props.selectAll(this.props.showUsers)} disdabled={this.props.showUsers.length > 0} checked={this.props.showUsers.length > 0 && this.props.showUsers.every(this.exist)} indeterminate={() => {
-                  const count = this.props.showUsers.filter(id => this.props.users.users.allUserInfo[id].select).length;
-                  return this.props.showUsers.length > 0 && 0 < count && count < this.props.showUsers.length;
+              <th className="user-list-table__checkbox" onClick={() => this.props.selectAll(filteredUsers)}>
+                <Checkbox onChange={() => this.props.selectAll(filteredUsers)} disdabled={this.props.showUsers.length > 0} checked={filteredUsers.length > 0 && filteredUsers.every(this.exist)} indeterminate={() => {
+                  const count = filteredUsers.filter(id => this.props.users.allUserInfo[id].select).length;
+                  return filteredUsers.length > 0 && 0 < count && count < filteredUsers.length;
                 }} />
               </th>
-              <th className="user-list-table__index">#</th>
-              <th>Name</th>
-              <th>Screen Name</th>
-              <th>Following</th>
-              <th>Followers</th>
-              <th>Last-update</th>
+              <SortThButton className="user-list-table__name" sortKey="name" name="Name" />
+              <SortThButton className="user-list-table__screen_name" sortKey="screen_name" name="Screen Name" />
+              <SortThButton className="user-list-table__following" sortKey="friends_count" name="Following" />
+              <SortThButton className="user-list-table__followers" sortKey="followers_count" name="Followers" />
+              <SortThButton className="user-list-table__last-update" sortKey="status.created_at" name="Last-update" />
             </tr>
           </thead>
           <tbody>
@@ -70,8 +119,10 @@ List.propTypes = {
 
 function mapStateToProps(state, ownProps) {
   return {
-    users: state.users,
+    users: state.users.users,
+    usersInitialized: state.users.isInitialized,
     settings: state.settings,
+    filter: state.filter.text,
     showUsers: (() => {
       switch(ownProps.type) {
       case 'selected':
@@ -86,7 +137,7 @@ function mapStateToProps(state, ownProps) {
       case 'kataomoware':
         return state.users.users.followers.filter(id => !state.users.users.allUserInfo[id].connections.includes('following'));
       }
-    }).call(this)
+    }).call(this).slice(0)
   };
 }
 
--- src/containers/Toolbar.js
+++ src/containers/Toolbar.js
@@ -2,25 +2,39 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { changeShowMode } from '../actions/settings';
 import { getUsersIfNeeded } from '../actions/users';
+import FilterInput from '../components/FilterInput';
+import SortButton from '../containers/SortButton';
 
 class Toolbar extends Component {
   render() {
     return (
-      <div className="btn-toolbar" role="toolbar">
-        <div className="btn-group" role="group">
-          <button className="btn btn-primary-outline" onClick={this.props.getUsers} disabled={this.props.isFetching}>
-            <i className={'fa fa-refresh ' + (this.props.isFetching ? 'fa-spin' : '')}></i>
-          </button>
+      <div className="clearfix">
+        <div className="pull-xs-left">
+          <div className="btn-toolbar" role="toolbar">
+            <div className="btn-group" role="group">
+              <button className="btn btn-primary-outline" onClick={this.props.getUsers} disabled={this.props.isFetching}>
+                <i className={'fa fa-refresh ' + (this.props.isFetching ? 'fa-spin' : '')}></i>
+              </button>
+            </div>
+          </div>
         </div>
-        <div className="btn-group" role="group" data-toggle="mode-switch">
-          <label className={'btn btn-primary-outline ' + (this.props.showMode === 'card' ? 'active' : '')} title="Show as a card">
-            <input type="radio" name="show_mode" value='card' onChange={this.props.changeShowMode} checked={this.props.showMode === 'card'} />
-            <i className="fa fa-th"></i>
-          </label>
-          <label className={'btn btn-primary-outline ' + (this.props.showMode === 'list' ? 'active' : '')} title="Show as a list">
-            <input type="radio" name="show_mode" value='list' onChange={this.props.changeShowMode} checked={this.props.showMode === 'list'} />
-            <i className="fa fa-list"></i>
-          </label>
+        <div className="pull-xs-right">
+          <div className="btn-toolbar" role="toolbar">
+            <div className="btn-group" role="group" data-toggle="mode-switch">
+              <label className={'btn btn-primary-outline ' + (this.props.showMode === 'card' ? 'active' : '')} title="Show as a card">
+                <input type="radio" name="show_mode" value='card' onChange={this.props.changeShowMode} checked={this.props.showMode === 'card'} />
+                <i className="fa fa-th"></i>
+              </label>
+              <label className={'btn btn-primary-outline ' + (this.props.showMode === 'list' ? 'active' : '')} title="Show as a list">
+                <input type="radio" name="show_mode" value='list' onChange={this.props.changeShowMode} checked={this.props.showMode === 'list'} />
+                <i className="fa fa-list"></i>
+              </label>
+            </div>
+            <SortButton />
+            <div className="btn-group" role="group">
+              <FilterInput />
+            </div>
+          </div>
         </div>
       </div>
     );

a2f482fc709ca0403901dffb1ac4976ed7a8ea11
sunya <mugen.xyz@gmail.com>
2016-04-28 00:51:48 +0900

    Add user information

--- controllers/isLogin.js
+++ controllers/isLogin.js
@@ -1,5 +1,11 @@
 exports.get = function *() {
-  this.body = JSON.stringify({
+  const res = {
     isLogin: !!this.req.user
-  });
+  };
+  if(this.req.user) {
+    res.name = this.req.user._json.name;
+    res.icon = this.req.user._json.profile_image_url_https;
+    res.screen_name = this.req.user._json.screen_name;
+  }
+  this.body = JSON.stringify(res);
 };

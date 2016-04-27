40cbb45011cf6ba9148f15933607d4bb6d4e4eb3
sunya <mugen.xyz@gmail.com>
2016-04-28 00:57:48 +0900

    Add username value

--- src/reducers/session.js
+++ src/reducers/session.js
@@ -2,19 +2,22 @@ import { FETCH_LOGIN_STATUS, RECEIVE_LOGIN_STATUS, FAILED_LOGIN_STATUS, REQUEST_
 
 export default (state = {
   isFetching: false,
-  isLogin: false
+  isLogin: false,
+  name: null,
+  icon: null,
+  screen_name: null
 }, action) => {
   switch(action.type) {
   case FETCH_LOGIN_STATUS:
     return Object.assign({}, state, {isFetching: true});
   case RECEIVE_LOGIN_STATUS:
-    return Object.assign({}, state, {isFetching: false, isLogin: action.isLogin});
+    return Object.assign({}, state, {isFetching: false, isLogin: action.isLogin, name: action.name, icon: action.icon, screen_name: action.screen_name});
   case FAILED_LOGIN_STATUS:
     return Object.assign({}, state, {isFetching: false});
   case REQUEST_LOGOUT:
     return Object.assign({}, state, {isFetching: true});
   case SUCCESS_LOGOUT:
-    return Object.assign({}, state, {isFetching: false, isLogin: action.ok});
+    return Object.assign({}, state, {isFetching: false, isLogin: action.ok, name: null, icon: null, screen_name: null});
   case FAILED_LOGOUT:
     return Object.assign({}, state, {isFetching: false});
   default:

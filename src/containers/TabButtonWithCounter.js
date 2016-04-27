debd35c09bc54b93ee5008af35fa9dd58597b286
sunya <mugen.xyz@gmail.com>
2016-04-27 22:48:00 +0900

    Improve counter

--- src/containers/TabButtonWithCounter.js
+++ src/containers/TabButtonWithCounter.js
@@ -3,27 +3,9 @@ import { connect } from 'react-redux';
 
 class TabButtonWithCounter extends Component {
   render() {
-    let count = 0;
-    switch(this.props.type) {
-    case 'selected':
-      count = Object.keys(this.props.allUserInfo).filter(id => this.props.allUserInfo[id].select).length;
-      break;
-    case 'friends':
-      count = this.props.friends.length;
-      break;
-    case 'followers':
-      count = this.props.followers.length;
-      break;
-    case 'kataomoi':
-      count = this.props.friends.filter(id => this.props.allUserInfo[id] && !this.props.allUserInfo[id].connections.includes('followed_by')).length;
-      break;
-    case 'kataomoware':
-      count = this.props.followers.filter(id => this.props.allUserInfo[id] && !this.props.allUserInfo[id].connections.includes('following')).length;
-      break;
-    }
     return (
       <li className="nav-item">
-        <a href={this.props.to} className={'nav-link ' + (this.props.type === 'friends' ? 'active' : '')} data-toggle="tab" role="tab">{this.props.text}({count})</a>
+        <a href={this.props.to} className={'nav-link ' + (this.props.to === '#friends' ? 'active' : '')} data-toggle="tab" role="tab" onClick={this.props.onClick}>{this.props.text}({this.props.count})</a>
       </li>
     );
   }
@@ -32,14 +14,29 @@ class TabButtonWithCounter extends Component {
 TabButtonWithCounter.propTypes = {
   to: PropTypes.string.isRequired,
   text: PropTypes.string,
-  type: PropTypes.oneOf(['selected', 'friends', 'followers', 'kataomoi', 'kataomoware']).isRequired,
-  allUserInfo: PropTypes.object.isRequired,
-  friends: PropTypes.array.isRequired,
-  followers: PropTypes.array.isRequired
+  count: PropTypes.number.isRequired,
+  onClick: PropTypes.func
 };
 
-function mapStateToProps(state) {
-  return {...state.users.users};
+function mapStateToProps(state, ownProps) {
+  return {
+    count: (() => {
+      switch(ownProps.to) {
+      case '#selected':
+        return Object.keys(state.users.users.allUserInfo).filter(id => state.users.users.allUserInfo[id].select).length;
+      case '#friends':
+        return state.users.users.friends.length;
+      case '#followers':
+        return state.users.users.followers.length;
+      case '#kataomoi':
+        return state.users.users.friends.filter(id => state.users.users.allUserInfo[id] && !state.users.users.allUserInfo[id].connections.includes('followed_by')).length;
+      case '#kataomoware':
+        return state.users.users.followers.filter(id => state.users.users.allUserInfo[id] && !state.users.users.allUserInfo[id].connections.includes('following')).length;
+      default:
+        return 0;
+      }
+    }).call(this)
+  };
 }
 
 export default connect(mapStateToProps)(TabButtonWithCounter);
